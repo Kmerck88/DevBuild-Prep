72b8f7638087f79a8c7f8abe439b90df3a3346be
Kmerck88 <kevin.merck88@gmail.com>
2021-07-21 18:25:09 -0400

    First four exercises

--- Program.cs
+++ Program.cs
@@ -6,17 +6,46 @@ namespace DevBuild_prep
     {
         static void Main(string[] args)
         {
-            //  Problem 4. Write a C# Sharp program to print the result of the specified operations.
-            // result is 23
-            Console.WriteLine(-1 + 4 * 6);
+         
+    // Problem 1 Write a C# Sharp program to print Hello and your name in a separate
 
-            // result is 5
-            Console.WriteLine((35 + 5) % 7);
+    // Console.WriteLine("Hello: \nKevin Merck!");  
+         
+         
+    // Problem 2. Write a C# Sharp program to print the sum of two numbers. 
 
-            // result is 12
-            Console.WriteLine(14 + -4 * 6 / 11);
-            // result is 3
-            Console.WriteLine(2 + 15 / 6 * 1 - 7 % 2);
+    //  int num1 = 5; 
+    //  int num2 = 10; 
+    //  Console.WriteLine(num1 + num2);  
+            
+         
+    // Problem 3. Write a C# Sharp program to print the result of dividing two numbers.
+
+    // int num1 = 50; 
+    // int num  = 10; 
+
+    // Console.WriteLine(50/10); 
+            
+            
+            
+     //  Problem 4. Write a C# Sharp program to print the result of the specified operations.
+    // result is 23
+    // Console.WriteLine(-1 + 4 * 6);
+
+    //  result is 5
+    // Console.WriteLine((35 + 5) % 7);
+
+    // result is 12
+    // Console.WriteLine(14 + -4 * 6 / 11);
+    
+    // /result is 3
+    // Console.WriteLine(2 + 15 / 6 * 1 - 7 % 2);
+
+    //  Expected Output:
+    // 23
+    // 5
+    // 12
+    // 3
 
 
 
