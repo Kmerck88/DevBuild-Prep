aca07518b073319e114f77c1c56771b2a2f33d60
Kmerck88 <kevin.merck88@gmail.com>
2021-07-21 17:55:29 -0400

    Problem 1

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DevBuild_prep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Problem 4. Write a C# Sharp program to print the result of the specified operations.
+            // result is 23
+            Console.WriteLine(-1 + 4 * 6);
+
+            // result is 5
+            Console.WriteLine((35 + 5) % 7);
+
+            // result is 12
+            Console.WriteLine(14 + -4 * 6 / 11);
+            // result is 3
+            Console.WriteLine(2 + 15 / 6 * 1 - 7 % 2);
+
+
+
+        }
+    }
+}
